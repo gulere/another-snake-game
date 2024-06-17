e116a379179b0c9da9b3f74f6d61535a39bacb6f
EcceEcce <163897149+gulere@users.noreply.github.com>
2024-06-17 11:54:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+# 100DOC: DAY 20+21 Snake Game
+
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+
+# Restart game
+def restart_game():
+    global game_is_on
+    game_is_on = True
+    snake.reset_body()
+    food.refresh()
+    scoreboard.reset_board()
+    game_loop()
+
+
+def game_loop():
+    global game_is_on
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
+
+        # Detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
+
+        # Detect collision with wall
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+            game_is_on = False
+            scoreboard.game_over()
+
+        # Detect collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("ece's snake game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(restart_game, "space")
+
+game_is_on = True
+game_loop()
+
+screen.exitonclick()
